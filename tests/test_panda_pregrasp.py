836adb76b776648684cc35bbb3c6d094677e6994
msabbah <msabbah@laas.fr>
2026-05-13 11:20:20 +0200

    fix reinstall

--- tests/test_panda_pregrasp.py
+++ tests/test_panda_pregrasp.py
@@ -17,6 +17,8 @@ def test_panda_pregrasp_solver_step() -> None:
     config.MPC.horizon = 8
     config.MPC.num_parallel_computations = 16
     config.MPC.num_control_points = 3
+    config.MPC.gain_samples_per_cycle = 8
+    config.MPC.gain_buffer_size = 8
 
     _, solver = build_model_and_solver(
         config,
